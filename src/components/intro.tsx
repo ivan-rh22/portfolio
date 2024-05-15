0815c037d8d37b75799b05cc09c74d470d162b82
Ricardo Ivan Hernandez <ricardo.hernandez21@utrgv.edu>
2024-05-15 00:58:12 -0500

    working intro section

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -25,7 +25,7 @@ export default function Intro() {
                 className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
                 >
                 <h1 className="mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
-                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-100 via-red-100 to-indigo-200">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-300 via-red-300 to-indigo-500 dark:from-blue-200 dark:via-red-200 dark:to-indigo-300">
                         I&apos;m{" "}
                     </span>
                     
@@ -59,12 +59,14 @@ export default function Intro() {
                     </Link>
                     <a
                         className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                        href="/files/resume.pdf" 
+                        target="_blank"
                     >
                         Download CV
                     </a>
                     <a
                         className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                        href="https://linkedin.com"
+                        href="https://www.linkedin.com/in/ricardo-hernandez-710aaa29a"
                         target="_blank"
                     >
                         <BsLinkedin />
@@ -72,7 +74,7 @@ export default function Intro() {
 
                     <a
                         className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-                        href="https://github.com"
+                        href="https://github.com/ivan-rh22"
                         target="_blank"
                     >
                         <FaGithubSquare />
